970f4cc71c0cb300a05b3fab96cae90e2108f0ef
Ari <arisanpala@gmail.com>
2025-02-17 12:55:13 +0100

    Segunda pre-entrega con corrección final

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,38 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getProducts, getProductsByCategory,} from "../../asincMock";
-import {ItemList} from "../ItemList/ItemList";
+import { getProducts, getProductsByCategory } from "../../asincMock";
+import { ItemList } from "../ItemList/ItemList";
 
+export const ItemListContainer = () => {
+  const { categoryId } = useParams();
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const asynFun = categoryId ? getProductsByCategory : getProducts;
 
+    asynFun(categoryId)
+      .then((resp) => {
+        setProducts(resp);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }, [categoryId]);
 
-export const ItemListContainer =()=>{
-    const {categoryId} = useParams();
-    const [products, setProducts] = useState([]);
-
-    useEffect(()=>{
-        const asynFun = categoryId ? getProductsByCategory : getProducts;
-
-        asynFun(categoryId)
-            .then((resp)=>{
-                setProducts(resp)
-            
-        })
-        .catch((error)=>{
-            console.log('Error', error)
-        })
-
-        
-    }, [categoryId])    
-
-
-
-
-
-    return(
-        <div className="container">
-            <ItemList products={products}/>  
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <ItemList products={products} />
+    </div>
+  );
+};
